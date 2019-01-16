3c79a0d01a6423171f1f55baa858834a47d342d9
Oluwafemi Shobande <oluwafemi.shobande@interswitchgroup.com>
2019-01-16 10:26:54 +0100

    Wallet creation now requires mutual consent between issuer and gateway node

--- issuer/src/main/kotlin/com/isw/paple/issuer/flows/CreateGatewayWallet.kt
+++ issuer/src/main/kotlin/com/isw/paple/issuer/flows/CreateGatewayWallet.kt
@@ -1,15 +1,11 @@
 package com.isw.paple.issuer.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import com.isw.paple.common.contracts.WalletContract
+import com.isw.paple.common.states.WalletState
 import com.isw.paple.common.types.Wallet
-import com.isw.paple.common.types.toState
 import com.isw.paple.common.utilities.getWalletStateByWalletId
-import net.corda.core.contracts.Command
-import net.corda.core.contracts.StateAndContract
 import net.corda.core.flows.*
 import net.corda.core.transactions.SignedTransaction
-import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
 /**
@@ -22,66 +18,96 @@ class CreateGatewayWallet(private val wallet: Wallet) : FlowLogic<SignedTransact
 
     companion object {
         // TODO: fine tune progress tracker
-        object NOTARY_ID : ProgressTracker.Step("Getting Notary Identity")
-        object TX_BUILDER : ProgressTracker.Step("Creating transaction builder, assigning notary, and gathering other components")
-
-        object TX_VERIFICATION : ProgressTracker.Step("Verifying transaction")
-        object TX_SIGNING : ProgressTracker.Step("Signing a transaction")
-        object FLOW_SESSION: ProgressTracker.Step("Initiation flow session with counter parties")
-        object FINALISING : ProgressTracker.Step("Finalising transaction") {
+        object FLOW_SESSION: ProgressTracker.Step("Initiation flow session with gateway party")
+        object VERIFYING_AND_SIGNING : ProgressTracker.Step("Verifying and signing transaction proposal") {
+            override fun childProgressTracker() = SignTransactionFlow.tracker()
+        }
+        object RECEIVE_FINALISED : ProgressTracker.Step("Receiving finalised transaction") {
             override fun childProgressTracker() = FinalityFlow.tracker()
         }
 
-        fun tracker() = ProgressTracker(NOTARY_ID, TX_BUILDER, TX_VERIFICATION, TX_SIGNING, FLOW_SESSION, FINALISING)
+        fun tracker() = ProgressTracker(
+            FLOW_SESSION,
+            VERIFYING_AND_SIGNING,
+            RECEIVE_FINALISED
+        )
     }
 
     override val progressTracker: ProgressTracker = tracker()
 
     @Suspendable
     override fun call(): SignedTransaction {
-        logger.info("Initiating CreateGatewayWallet flow")
-        val walletId = wallet.walletId
-
-        logger.info("Checking for existence of state for $wallet.")
-        val result = getWalletStateByWalletId(walletId, serviceHub)
-
-        if (result != null) {
-            val linearId = result.state.data.linearId
-            throw IllegalArgumentException("Wallet $walletId already exists with linearId ($linearId).")
-        }
-
-        logger.info("No state for $wallet. Adding it.")
-        val walletState = wallet.toState(ourIdentity)
-        progressTracker.currentStep = NOTARY_ID
-        val notary = serviceHub.networkMapCache.notaryIdentities.first()
-
-        // The node running this flow is always the only signer.
-        val command = Command(WalletContract.Add(), listOf(ourIdentity.owningKey))
-        val outputStateAndContract = StateAndContract(walletState, WalletContract.CONTRACT_ID)
-        progressTracker.currentStep = TX_BUILDER
-        val unsignedTransaction = TransactionBuilder(notary = notary).withItems(command, outputStateAndContract)
-
-        // TODO: verify transaction
-
-        progressTracker.currentStep = TX_SIGNING
-        val signedTransaction = serviceHub.signInitialTransaction(unsignedTransaction)
 
         progressTracker.currentStep = FLOW_SESSION
-        val session = initiateFlow(ourIdentity)
+        // initiate session with gateway party and send proposed wallet data
+        val gatewaySession = initiateFlow(wallet.owner)
+        gatewaySession.send(wallet)
 
-        progressTracker.currentStep = FINALISING
-        return subFlow(FinalityFlow(signedTransaction, session))
+        // Verify and sign the transaction.
+        progressTracker.currentStep = VERIFYING_AND_SIGNING
 
-    }
+        val signTransactionFlow = object : SignTransactionFlow(gatewaySession, VERIFYING_AND_SIGNING.childProgressTracker()) {
+            override fun checkTransaction(stx: SignedTransaction) {
 
-}
+                val ledgerTransaction = stx.toLedgerTransaction(serviceHub, false)
+                val outputState = ledgerTransaction.outputsOfType<WalletState>().single()
+                val linearId = outputState.linearId
 
-@InitiatedBy(CreateGatewayWallet::class)
-class CreateGatewayWalletResponder(private val counterParty: FlowSession) : FlowLogic<Unit>() {
+                //check if a state with same walletId exists in the vault
+                logger.info("Checking for existence of state for $outputState.")
+                val result = getWalletStateByWalletId(walletId = linearId.externalId.toString(), services = serviceHub)
 
-    @Suspendable
-    override fun call() {
-        subFlow(ReceiveFinalityFlow(counterParty))
+                //if state already exists
+                if (result != null) {
+                    throw FlowException("Wallet ${linearId.externalId} already exists with linearId ($linearId).")
+                }
+
+                require(gatewaySession.counterparty == outputState.owner) {"Yes!"}
+            }
+        }
+        val transactionId = subFlow(signTransactionFlow).id
+
+        progressTracker.currentStep = RECEIVE_FINALISED
+        return subFlow(ReceiveFinalityFlow(gatewaySession, expectedTxId = transactionId))
+
+//        //construct state
+//        val walletState = wallet.toState(createdBy = ourIdentity)
+//
+//        logger.info("Selecting Notary")
+//        //select notary
+//        progressTracker.currentStep = NOTARY_ID
+//        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+//
+//        logger.info("Building transaction")
+//        //select command with list of required signers
+//        val command = Command(WalletContract.Create(), listOf(ourIdentity.owningKey, wallet.owner.owningKey))
+//        //attach contract to constructed state
+//        val outputStateAndContract = StateAndContract(walletState, WalletContract.CONTRACT_ID)
+//
+//        //build transaction with various components
+//        progressTracker.currentStep = TX_BUILDER
+//        val unsignedTransaction = TransactionBuilder(notary = notary).withItems(command, outputStateAndContract)
+//
+//        logger.info("Verifying transaction")
+//        progressTracker.currentStep = TX_VERIFICATION
+//        unsignedTransaction.verify(serviceHub)
+//
+//        logger.info("Signing the transaction ourselves")
+//        progressTracker.currentStep = TX_SIGNING
+//        val partiallySignedTransaction = serviceHub.signInitialTransaction(unsignedTransaction)
+//
+//        logger.info("Starting flow sessions with counter parties")
+//        progressTracker.currentStep = FLOW_SESSION
+//        val counterpartySession = initiateFlow(wallet.owner)
+//
+//        logger.info("Gathering signatures form counterparties")
+//        progressTracker.currentStep = GATHERING_SIGNATURES
+//        val signedTransaction = subFlow(CollectSignaturesFlow(partiallySignedTransaction, listOf(counterpartySession)))
+//
+//        logger.info("finalising transaction")
+//        progressTracker.currentStep = FINALISING
+//        return subFlow(FinalityFlow(transaction = signedTransaction, sessions = listOf(counterpartySession)))
+
     }
 
 }
