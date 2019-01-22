1d95c206083230571460b2119524d3ff076d1e35
Oluwafemi Shobande <oluwafemi.shobande@interswitchgroup.com>
2019-01-22 12:42:40 +0100

    Seperation of contract logic for different wallet kinds

--- common/src/main/kotlin/com/isw/paple/common/contracts/WalletContract.kt
+++ common/src/main/kotlin/com/isw/paple/common/contracts/WalletContract.kt
@@ -1,6 +1,7 @@
 package com.isw.paple.common.contracts
 
 import com.isw.paple.common.states.WalletState
+import com.isw.paple.common.types.WalletStatus
 import com.isw.paple.common.types.WalletType
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
@@ -23,10 +24,10 @@ class WalletContract : Contract {
     // TODO: Implement Contract Code
     override fun verify(tx: LedgerTransaction) {
         val walletCommand = tx.commands.requireSingleCommand<Commands>()
-        val setOfSigners = walletCommand.signers.toSet()
+        val signers = walletCommand.signers.toSet()
 
         when (walletCommand.value) {
-            is Create -> verifyCreate(tx, setOfSigners)
+            is Create -> verifyCreate(tx, signers)
 //            is Update -> verifyUpdate(tx, setOfSigners)
             else -> throw IllegalArgumentException("Unrecognised command.")
         }
@@ -34,19 +35,29 @@ class WalletContract : Contract {
 
     private fun verifyCreate(tx: LedgerTransaction, signers: Set<PublicKey>) = requireThat {
         //Shape constraints
-        "Transaction must have zero input states" using (tx.inputStates.isEmpty())
+        "Tx must have zero input states" using (tx.inputStates.isEmpty())
         "Tx must have one output state" using (tx.outputStates.size == 1)
 
         //Wallet specific constraints
         val walletState = tx.outputStates.single() as WalletState
         "Wallet creation and funding must be done separately. Hence, amount must be zero" using (walletState.amount.quantity == 0L)
+
+        // TODO: find better way of doing this
         "Only NGN and USD are supported" using (walletState.amount.token.toString() in allowedCurrencies)
-        if (walletState.type == WalletType.GATEWAY_OWNED) {
-            "If wallet type is GatewayOwned, Issuer cannot be owner of wallet" using (walletState.owner != walletState.createdBy)
+
+        "Wallet must be unverified" using (!walletState.verified)
+
+        when (walletState.type) {
+            WalletType.GATEWAY_OWNED -> {
+                "If wallet type is GatewayOwned, Issuer cannot be owner of wallet" using (walletState.owner != walletState.createdBy)
+                "There must be 2 signers" using (signers.size == 2)
+            }
+            else -> {
+                "Other than GatewayOwned wallet, Owner and CreatedBy must be same" using (walletState.owner == walletState.createdBy)
+                "There must be 1 signer" using (signers.size == 1)
+            }
         }
 
-        //signer constraints
-        "There must be 2 signers" using (signers.size == 2)
         "The creator of this wallet (i.e the issuer) must sign the transaction" using (signers.contains(walletState.createdBy.owningKey))
         "Wallet owner must sign this transaction" using (signers.contains(walletState.owner.owningKey))
     }
