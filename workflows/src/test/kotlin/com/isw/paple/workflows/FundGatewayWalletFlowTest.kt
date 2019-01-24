3a0fc1bfbce9a93f27783fb783b1e4668c51fddc
Oluwafemi Shobande <oluwafemi.shobande@interswitchgroup.com>
2019-01-24 15:15:34 +0100

    Cash states are only stored in the vault of the owner, hence in the test we limit assertions to the gatewaynode (being the owner)

--- workflows/src/test/kotlin/com/isw/paple/workflows/FundGatewayWalletFlowTest.kt
+++ workflows/src/test/kotlin/com/isw/paple/workflows/FundGatewayWalletFlowTest.kt
@@ -1,12 +1,15 @@
 package com.isw.paple.workflows
 
+import com.isw.paple.common.states.IssuanceState
 import com.isw.paple.common.states.WalletState
 import com.isw.paple.common.types.Wallet
 import com.isw.paple.common.types.WalletStatus
 import com.isw.paple.common.types.WalletType
 import net.corda.core.contracts.Amount
+import net.corda.core.contracts.Issued
 import net.corda.core.node.services.queryBy
 import net.corda.finance.USD
+import net.corda.finance.contracts.asset.Cash
 import net.corda.testing.core.singleIdentity
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -21,26 +24,37 @@ class FundGatewayWalletFlowTest: FlowTestsBase() {
         val balance = Amount(0, USD)
         val status = WalletStatus.UNKNOWN
         val type = WalletType.GATEWAY_OWNED
-
         val wallet = Wallet(walletId, gatewayParty, balance, status, type)
 
         gatewayNodeAddsRecognisedIssuer(issuer, gatewayNode)
-        issuerNodeCreatesWallet(wallet)
-        val fundAmount =Amount(10, USD)
+        val createdWalletState = issuerNodeCreatesWallet(wallet).tx.outputsOfType<WalletState>().single()
+
+        val fundAmount =Amount(10*100, USD)
         issuerNodeFundsGatewayWallet(walletId, fundAmount)
 
         for (node in listOf(issuerNode, gatewayNode)) {
             node.transaction {
                 val walletStates = node.services.vaultService.queryBy<WalletState>().states
-                assertEquals(1, walletStates.size)
-                val walletState = walletStates.single().state.data
+                val issuanceStates = node.services.vaultService.queryBy<IssuanceState>().states
 
-                assertEquals(walletState.balance, balance.plus(fundAmount))
-                assertEquals(walletState.createdBy, issuer)
-                assertEquals(walletState.owner, gatewayParty)
-                assertEquals(walletState.status, status)
-                assertEquals(walletState.verified, false)
-                assertEquals(walletState.type, type)
+                assertEquals(1, walletStates.size)
+                assertEquals(1, issuanceStates.size)
+
+                val walletState = walletStates.single().state.data
+                val issuanceState = issuanceStates.single().state.data
+
+                assertEquals(walletState, createdWalletState.withNewBalance(createdWalletState.balance.plus(fundAmount)))
+                val balanceDiff = walletState.balance.minus(createdWalletState.balance)
+                assertEquals(balanceDiff, issuanceState.amount)
+                assertEquals(issuanceState.recipient, walletState.owner)
+
+                if (node.info.singleIdentity() == walletState.owner) {
+                    val cashStates = node.services.vaultService.queryBy<Cash.State>().states
+                    assertEquals(1, cashStates.size)
+                    val cashState = cashStates.single().state.data
+                    assertEquals(walletState.owner, cashState.owner)
+                    assertEquals(cashState.amount, Amount(balanceDiff.quantity, Issued(issuanceState.issuer.ref(0), balanceDiff.token)))
+                }
             }
         }
     }
